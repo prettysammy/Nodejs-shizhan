6659a28e8f95afeeacb18d8e0e409a3497e16680
prettysammy <xuyanwr@126.com>
2017-08-14 01:48:34 +0800

    compelete

--- ChartingRoom/lib/chat_server.js
+++ ChartingRoom/lib/chat_server.js
@@ -6,6 +6,59 @@ var nickNames = {};
 var namesUsed = {};
 var currentRoom = {};
 
+var socket = io.connect();
+$(document).read(function(){
+	var chatApp = new Chat(socket);
+
+	socket.on('nameResult',function(result){
+		var message;
+
+		if(result.success){
+			message = 'You are now known as' result.name + '.';
+		}else{
+			message = result.message;
+		}
+		$('#messages').append(divSystemContentElement(message));
+	});
+
+	socket.on('joinResult',function(result){
+		$('#room').text(resut.room);
+		$('#messages').append(divSystemContentElement('Room changed.'));
+	});
+
+	socket.on('message',function(message){
+		var newElement = $('<div></div').text(message.text);
+		$('#messages').append(newElement);
+	});
+
+	socket.on('rooms', function(rooms){
+		$('#room-ist').empty();
+
+		for(var room in rooms){
+			room = room.substring(1,room.length);
+			if(room != ''){
+				$('#room-list').append(divEscapedContentElement(room));
+			}
+		}
+
+		$('#room-list div').click(function(){
+			chatApp.processCommand('/join' + $(this).text());
+			$("#send-message").focus();			
+		});
+	});
+
+	setInterval(function(){
+		socket.emit('rooms');
+	},1000);
+
+	$('#send-message').focus();
+
+	$('#send-from').submit(function(){
+		processUserInput(chatApp,socket);
+		return false;
+	});
+
+});
 
 exports.listen = function(server){
 	io = socketio.listen(server);
@@ -37,7 +90,7 @@ function assignGuestName(socket,guestNumber,nickNames,namesUsed){
 	});
 	namesUsed.push(name);
 	return guestNumber+1;
-}
+};
 
 function joinRoom(socket,room){
 	socket.join(room);
@@ -62,9 +115,9 @@ function joinRoom(socket,room){
 		usersInRoomSummary +='.';
 		socket.emit('message',{text:usersInRoomSummary});
 	}
-}
+};
 
-	function handleNameChangeAttempts(socket,nickNames,namesUsed){
+function handleNameChangeAttempts(socket,nickNames,namesUsed){
 		socket.on('nameAttempt',function(name){
 			if(name.indexOf('Guest') == 0){
 				socket.emit('nameResut',{
@@ -94,9 +147,9 @@ function joinRoom(socket,room){
 				}
 			}
 		});
-	}
+};
 
-	function handleMessageBoardcasting(socket){
+function handleMessageBoardcasting(socket){
 		socket.on('message',function(message){
 			socket.broadcast.to(message.room).emit('message',{
 				text:nickNames[socket.id]+':'+message.text
@@ -117,7 +170,7 @@ function joinRoom(socket,room){
 			delete namesUsed[nameIndex];
 			delete nickNames[socket.id];
 		});
-	}
+};
 
 
 }
--- ChartingRoom/server.js
+++ ChartingRoom/server.js
@@ -54,6 +54,7 @@ var server = http.createServer(function(request,response){
 	serverStatic(response,cache,absPath);
 })
 
+
 server.listen(3000,function(){
 	console.log("Server listening on port 3000");
 });
