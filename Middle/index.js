6c0511878ae230e271f09f737495c5e2b63f7e99
prettysammy <xuyanwr@126.com>
2017-08-23 02:23:21 +0800

    middle

---
+++ Middle/index.js
@@ -0,0 +1,15 @@
+var connect = require('connect');
+var app = connect();
+
+function logger(req,res,next){
+	console.log('%s %s',req.method,req.url);
+	next();
+}
+
+function hello(req,res){
+	res.setHeader('Content-Type','text/plain');
+	res.end('hello world');
+}
+
+app.use(logger).use(hello);
+app.listen(3000);
